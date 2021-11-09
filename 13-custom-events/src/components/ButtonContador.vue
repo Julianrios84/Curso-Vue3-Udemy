135d13fae77ca5cc57ae5c53f3a5f3d6ccc3add4
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2021-11-09 00:03:56 -0500

    Eventos personalizados

---
+++ 13-custom-events/src/components/ButtonContador.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="btn-contador">
+    <button @click="execAction" >{{ texto }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    texto: {
+      type: String,
+      default: "Aumentar",
+    },
+  },
+  methods: {
+    execAction() {
+      this.$emit("action")
+    },
+  }
+};
+</script>
+
+<style></style>
---
+++ 13-custom-events/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>Contador: {{ contador }}</h1>
+    <button-contador @action="handleIncrement" />
+    <button-contador @action="handleDecrement" texto="Disminuir" />
+  </div>
+</template>
+
+<script>
+import ButtonContador from "./ButtonContador.vue";
+export default {
+  components: { ButtonContador },
+  name: "HelloWorld",
+  props: {
+    contador: Number,
+  },
+  data: () => ({
+    contador: 0,
+  }),
+  methods: {
+    handleIncrement() {
+      this.contador++
+    },
+    handleDecrease() {
+      this.contador--
+    }
+  }
+};
+</script>
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
