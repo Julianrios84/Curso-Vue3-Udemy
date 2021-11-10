76d3ffe2008e3a10e01690acf4ded9b0c55013d4
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2021-11-09 22:24:20 -0500

    aprendiendo a integrar Bootstrap, a trabajar con Rutas

---
+++ 14-gif-route/src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="footer">
+    <footer class="text-center bg-dark text-white py-2 bg-gradient">
+      <h4>&copy; {{ year }} - Julian Esteban</h4>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    year: new Date().getFullYear(),
+  }),
+};
+</script>
+
+<style>
+.footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+</style>
